5e5c211239cb2a83dd26e3f535fae8aab11e495c
metehan-karaca <mmetehank@gmail.com>
2025-04-24 14:38:54 +0300

    feat: appointment smart button add without functionality

--- models/appointment_inherit.py
+++ models/appointment_inherit.py
@@ -1,9 +1,34 @@
 from odoo import models, fields, api
 
+#I chose to inherit appointment model to make the additions instructed in chapter 2
 class HospitalAppointment(models.Model):
     _inherit = 'hospital.appointment'  # inherits my appointment.py
 
     total_amount = fields.Char(string="Total Amount")
+    pending_amount = fields.Char(string="Pending Amount")
+
+    sale_order_lines = fields.One2many('sale.order.line', 'appointment_id', string="Sale Order Lines")
+
+    def sale_orders_smart_button(self):
+        return True
+    
+    def invoice_smart_button(self):
+        return True
+    
+    def payment_smart_button(self):
+        return True
+
+
+
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    appointment_id = fields.Many2one('hospital.appointment', string="Appointment")
+
+
+
 
 
 
